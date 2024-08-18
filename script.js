b26a57385b6add8aaf7cd88dcef95393d92d5f7d
khaled <kh2662174@gmail.com>
2024-08-18 08:35:03 +0300

    chang Style

--- script.js
+++ script.js
@@ -10,48 +10,93 @@ const numberR = document.querySelector("#number");
 const guessNumber = document.querySelector("#numberInput");
 const num1 = 3;
 
+const resultBox = document.querySelector(".result");
+
 let RealNumber;
 let score = 100;
 
+// Level 1
 const easyMode = () => {
   resetGame();
+  resetStyle();
+  document.querySelector("#easy").style.backgroundColor = "black";
   RealNumber = getRandomNumber(0, 20);
-  min_max.textContent = "0 - 20";
+  console.log(RealNumber);
+  min_max.textContent = "Guess Number : 0 - 20";
 };
+// Level 2
 const middleMode = () => {
   resetGame();
+  resetStyle();
+  document.querySelector("#middel").style.backgroundColor = "black";
   RealNumber = getRandomNumber(0, 30);
   console.log(RealNumber);
-  min_max.textContent = "0 - 30";
+  min_max.textContent = "Guess Number  : 0 - 30";
 };
+// Level 3
 const hardMode = () => {
   resetGame();
+  resetStyle();
+  document.querySelector("#hard").style.backgroundColor = "black";
   RealNumber = getRandomNumber(0, 50);
-  min_max.textContent = "0 - 50";
+  console.log(RealNumber);
+  min_max.textContent = "Guess Number : 0 - 50";
 };
 
+// Rest Game
 const resetGame = () => {
   score = 100;
   scoreText.textContent = `Score : ${score}`;
   document.querySelector("#number").textContent = `??`;
   numberR.textContent = `?? `;
+  guessNumber.value = null;
 };
+// chang Style after win
+const changStyleeInWin = () => {
+  document.querySelector("body").style.backgroundColor = "#08da19";
+  numberR.style.color = "#08da19";
+  scoreText.style.fontSize = "50px";
+  scoreText.style.color = "black";
+  scoreText.style.fontWeight = "1000";
+};
+// reset Style
+const resetStyle = () => {
+  document.querySelector("body").style.backgroundColor = "#302e2e";
+  numberR.style.color = "beige";
+  scoreText.style.fontSize = "26px";
+  scoreText.style.color = "beige";
 
+  document.querySelector("#easy").style.backgroundColor = "#302e2e";
+  document.querySelector("#middel").style.backgroundColor = "#302e2e";
+  document.querySelector("#hard").style.backgroundColor = "#302e2e";
+};
+//
+//
+//
+//
 function checkNumber() {
   const numberValue = parseInt(guessNumber.value, 10);
 
   console.log(`the Real Number = ${RealNumber}`);
   console.log(`the guess number = ${numberValue}`);
 
-  if (numberValue === RealNumber) {
-    return (document.querySelector("#number").textContent = `${RealNumber}`);
+  if (!numberValue) {
+    return;
+  }
+  // if win game
+  else if (numberValue === RealNumber) {
+    changStyleeInWin();
+    document.querySelector("#number").textContent = ` \n\ Win `;
+    return;
   } else if (numberValue > RealNumber) {
-    numberR.textContent = `Down  `;
+    numberR.textContent = `Down ↓`;
   } else {
-    numberR.textContent = `UP ^ `;
+    numberR.textContent = `UP ↑ `;
   }
   score -= 5;
-  scoreText.textContent = `Score : ${score}`;
+  score < 60
+    ? (scoreText.textContent = `Score : ${score} 😡`)
+    : (scoreText.textContent = `Score : ${score}`);
 }
 
 easyMode();
